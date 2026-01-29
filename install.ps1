ebc315103a8f6888c223b073200fbcc7f13a1435
Takeru O'oyama <tqer39@gmail.com>
2026-01-30 05:19:38 +0900

    Fix winget exit code formatting (#198)

--- install.ps1
+++ install.ps1
@@ -525,7 +525,7 @@ function Install-WingetPackages {
             try {
                 $result = winget install --id $package --accept-source-agreements --accept-package-agreements --silent 2>&1
                 if ($LASTEXITCODE -ne 0) {
-                    $exitCodeHex = ('0x{0:X8}' -f [uint32]$LASTEXITCODE)
+                    $exitCodeHex = ('0x{0:X8}' -f [uint32]($LASTEXITCODE -band 0xFFFFFFFF))
                     $nonFatalExitCodes = @(
                         '0x8A15002B' # Winget sometimes returns this for transient/unavailable installer states.
                     )
